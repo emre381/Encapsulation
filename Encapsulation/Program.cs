93c9197aecdbd2c18f5437e78a897f8f7c9a98c8
emre381 <cihatemre381@gmail.com>
2024-09-27 18:53:52 +0300

    first commit

---
+++ Encapsulation/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Encapsulation
+{
+     class Program
+    {
+        class SoccerPlayer
+        {
+            private int bonservicePrice;
+            public int getBonservicePrice()
+            {
+                return bonservicePrice;
+            }
+            public void setBonservicePrice(int price)
+            {
+                this.bonservicePrice = price;
+            }
+        }
+        static void Main(string[] args)
+        {
+            SoccerPlayer player = new SoccerPlayer();
+            player.setBonservicePrice(25000000);
+
+            Console.WriteLine(player.getBonservicePrice());
+            Console.ReadLine();
+        }
+    }
+}
